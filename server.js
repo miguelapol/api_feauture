4300c4f472b4074d94f974e5bee0d481bfebde98
miguelapol <apolonio1995@gmail.com>
2022-05-09 12:11:37 -0500

    se modifico para que se pueda modificar la inscription en el put

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const port = process.env.PORT || 3000;
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+const cors=require("cors");
+const corsOptions = {
+    origin: "http://localhost:8081"
+}
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.json({message: 'alive'});
@@ -39,7 +44,8 @@ app.put('/commanders/:id', async (req, res) => {
       data:{  
               name:req.body.name,
               username:req.body.username,
-              mainstack: req.body.mainstack
+              mainstack: req.body.mainstack,
+              inscription: req.body.inscription
             
       }
   })
